f444e6f4f24b4cdddf032ebd7aa3d1dab1270c36
Andy Hartono <andy.hartono@outlook.com>
2021-02-20 09:58:29 +0700

    rewrite creating HTML component, instead of using functions, using innerHTML is shorter and good enough

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,15 @@ const galleryDiv = document.getElementById('gallery')
  * @param {object} properties - properties of the new element
  * @returns {element}
  */
-function createElement(elementType, properties) {
-    const element = document.createElement(elementType)
+// function createElement(elementType, properties) {
+//     const element = document.createElement(elementType)
 
-    for (const key in properties) {
-        element[key] = properties[key]
-    }
-    return element
-}
+//     for (const key in properties) {
+//         element[key] = properties[key]
+//     }
+//     return element
+// }
+
 
 /**
  * This function will first call createElement() function to create new child Element.
@@ -28,147 +29,59 @@ function createElement(elementType, properties) {
  * @param {object} childProp - properties of the child element
  * @returns {element}
  */
-function appendTo(parentEl, childEl, childProp) {
-    const element = createElement(childEl, childProp)
-    parentEl.appendChild(element)
+// function appendTo(parentEl, childEl, childProp) {
+//     const element = createElement(childEl, childProp)
+//     parentEl.appendChild(element)
 
-    return element
-}
+//     return element
+// }
 
-const searchForm = appendTo(searchContainerDiv[0], 'form', {
-    action: '#',
-    method: 'get'
-})
 
-// const searchInput = 
-appendTo(searchForm, 'input', {
-    type: 'search',
-    id: 'search-input',
-    className: 'search-input',
-    placeholder: 'Search ...'
-})
+searchContainerDiv[0].innerHTML = `    
+    <form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>
+`
 
-// const searchSubmit = 
-appendTo(searchForm, 'input', {
-    type: 'submit',
-    value: '&#x1F50D;',
-    id: 'search-submit',
-    className: 'search-submit'
-})
 
-const cardDiv = appendTo(galleryDiv, 'div', {
-    className: 'card'
-})
+galleryDiv.innerHTML = `
+    <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">first last</h3>
+            <p class="card-text">email</p>
+            <p class="card-text cap">city, state</p>
+        </div>
+    </div>
+`
 
-const cardImageDiv = appendTo(cardDiv, 'div', {
-    className: 'card-img-container'
-})
 
-// const cardImg = 
-appendTo(cardImageDiv, 'img', {
-    className: 'card-img',
-    src: 'https://placehold.it/90x90',
-    alt: 'profile picture'
-})
+body.innerHTML = `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                <h3 id="name" class="modal-name cap">name</h3>
+                <p class="modal-text">email</p>
+                <p class="modal-text cap">city</p>
+                <hr>
+                <p class="modal-text">(555) 555-5555</p>
+                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+        </div>
 
-const cardInfoDiv = appendTo(cardDiv, 'div', {
-    className: 'card-info-container'
-})
+        // IMPORTANT: Below is only for exceeds tasks 
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>
+`
 
-// const nameH3 = 
-appendTo(cardInfoDiv, 'h3', {
-    id: 'name',
-    className: 'card-name cap',
-    textContent: 'first last'
-})
-
-// const emailP = 
-appendTo(cardInfoDiv, 'p', {
-    className: 'card-text',
-    textContent: 'email'
-})
-
-// const cityP = 
-appendTo(cardInfoDiv, 'p', {
-    className: 'card-text cap',
-    textContent: 'city, state'
-})
-
-const modalContainerDiv = appendTo(body, 'div', {
-    className: 'modal-container'
-})
-
-const modalDiv = appendTo(modalContainerDiv, 'div', {
-    className: 'modal'
-})
-
-// const modalCloseBtn = 
-appendTo(modalDiv, 'button', {
-    type: 'button',
-    id: 'modal-close-btn',
-    className: 'modal-close-btn',
-    innerHTML: '<strong>X</strong>'
-})
-
-const modalInfoDiv = appendTo(modalDiv, 'div', {
-    className: 'modal-info-container'
-})
-
-appendTo(modalInfoDiv, 'img', {
-    className: 'modal-img',
-    src: 'https://placehold.it/125x125',
-    alt: 'profile picture'
-})
-
-appendTo(modalInfoDiv, 'h3', {
-    id: 'name',
-    className: 'modal-name cap',
-    textContent: 'name'
-})
-
-appendTo(modalInfoDiv, 'p', {
-    className: 'modal-text',
-    textContent: 'email'
-})
-
-appendTo(modalInfoDiv, 'p', {
-    className: 'modal-text cap',
-    textContent: 'city'
-})
-
-appendTo(modalInfoDiv, 'hr')
-
-appendTo(modalInfoDiv, 'p', {
-    className: 'modal-text',
-    textContent: '(555) 555-5555'
-})
-
-appendTo(modalInfoDiv, 'p', {
-    className: 'modal-text',
-    textContent: '123 Portland Ave., Portland, OR 97204'
-})
-
-appendTo(modalInfoDiv, 'p', {
-    className: 'modal-text',
-    textContent: 'Birthday: 10/21/2015'
-})
-
-const modalBtnContainerDiv = appendTo(modalContainerDiv, 'div', {
-    className: 'modal-btn-container'
-})
-
-appendTo(modalBtnContainerDiv, 'button', {
-    type: 'button',
-    id: 'modal-prev',
-    className: 'modal-prev btn',
-    textContent: 'Prev'
-})
-
-appendTo(modalBtnContainerDiv, 'button', {
-    type: 'button',
-    id: 'modal-next',
-    className: 'modal-next btn',
-    textContent: 'Next'
-})
 
 //#endregion
