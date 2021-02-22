9dc2b1d35a4906c6b6974e288ac661b6e9e4222e
Andy Hartono <andy.hartono@outlook.com>
2021-02-22 13:02:25 +0700

    finish working on this project

--- js/scripts.js
+++ js/scripts.js
@@ -1,179 +1,221 @@
-//#region Selecting HTML components ***************************
+//#region Select HTML components & define variables ***********
 const body = document.querySelector('body')
 const searchContainerDiv = document.getElementsByClassName('search-container')
 const galleryDiv = document.getElementById('gallery')
+
+let modalEmpIndex
 //#endregion
 
-//#region Generate Search Form ********************************
-/**
- * Create HTML Elements
- * @param {string} elementType - The type of element to be created 
- * @param {object} properties - properties of the new element
- * @returns {element}
- */
-function createElement(elementType, properties) {
-    const element = document.createElement(elementType)
-
-    for (const key in properties) {
-        element[key] = properties[key]
-    }
-    return element
-}
-
-/**
- * This function will first call createElement() function to create new child Element.
- * And after that, append that child element to the parent element
- * @param {element} parentEl - parent HTML Element
- * @param {string} childEl  - element type to be created
- * @param {object} childProp - properties of the child element
- * @returns {element}
- */
-function appendTo(parentEl, childEl, childProp) {
-    const element = createElement(childEl, childProp)
-    parentEl.appendChild(element)
-
-    return element
-}
-
-
+//#region Generate Basic HTML Structure ***********************
+// 1. Search Form
 searchContainerDiv[0].innerHTML = `
     <form action="#" method="get">
         <input type="search" id="search-input" class="search-input" placeholder="Search...">
-        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
     </form>
 `
+const searchInput = document.getElementById('search-input')
+
+
+// 2. Pop-up Modal
+body.insertAdjacentHTML('beforeend',`
+    <div id="modal-container" class="modal-container" hidden="true">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div id="modal-info-container" class="modal-info-container">
+            </div>
+        </div>
+
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>
+`)
+
+const modalContainerDiv = document.getElementById('modal-container')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const modalInfoContainerDiv = document.getElementById('modal-info-container')
+const modalPrevBtn = document.getElementById('modal-prev')
+const modalNextBtn = document.getElementById('modal-next')
 //#endregion
 
-
 //#region Fetch Data from https://randomuser.me/ **************
+let employeeList = []
+let employeeQty = 0
 
-const userData = fetch('https://randomuser.me/api/?results=12')
-                    .then((response) => response.json())
-                    .then((data) => generateEmpCard(data.results))
+async function fetchData(url) {
+    try {
+        const response = await fetch(url)
+        const json = await response.json()
+        return json
+    }
+    catch (err) {
+        throw `${err} (${url})`
+    }
+}
 
 
 function generateEmpCard(arr) {
     arr.map(employee => {
-        const cardDiv = appendTo(galleryDiv, 'div', {
-            className: 'card'
-        })
-
-        const cardImageDiv = appendTo(cardDiv, 'div', {
-            className: 'card-img-container'
-        })
-
-        appendTo(cardImageDiv, 'img', {
-            className: 'card-img',
-            src: employee.picture.medium,
-            alt: `${employee.name.title} ${employee.name.first} ${employee.name.last}`
-        })
-
-        const cardInfoDiv = appendTo(cardDiv, 'div', {
-            className: 'card-info-container'
-        })
-
-        appendTo(cardInfoDiv, 'h3', {
-            id: 'name',
-            className: 'card-name cap',
-            textContent: `${employee.name.first} ${employee.name.last}`
-        })
-
-        appendTo(cardInfoDiv, 'p', {
-            className: 'card-text',
-            textContent: `${employee.email}`
-        })
-
-        appendTo(cardInfoDiv, 'p', {
-            className: 'card-text cap',
-            textContent: `${employee.location.city} ${employee.location.state}`
-        })
+        galleryDiv.insertAdjacentHTML('beforeend',`
+            <div class="card" id=${employee.email}>
+                <div class="card-img-container">
+                    <img class="card-img" src=${employee.picture.medium} alt="${employee.name.title} ${employee.name.first} ${employee.name.last} Photo">
+                </div>
+                <div class="card-info-container">
+                    <h3 class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <p class="card-text">${employee.email}</p>
+                    <p class="card-text cap">${employee.location.city}</p>
+                </div>
+            </div>
+        `)
     })
 }
 
 
+fetchData('https://randomuser.me/api/?results=12&nat=au,gb,nz,us')
+    .then((data) => {
+        employeeList = data.results
+        employeeQty = employeeList.length
+    })
+    .then(() => generateEmpCard(employeeList))
+//#endregion
+
+//#region Helper Function for Event Listener ******************
+function generateModal(i) {
+    const pic = employeeList[i].picture.large
+    const name = `${employeeList[i].name.first} ${employeeList[i].name.last}`
+    const email = employeeList[i].email
+    const city = employeeList[i].location.city
+    const cellNo = formatCellNo(employeeList[i].cell)
+    const address = `${employeeList[i].location.street.number} ${employeeList[i].location.street.name}, ${employeeList[i].location.state} ${employeeList[i].location.postcode}`
+    const birthday = `Birthday: ${formatBirthday(employeeList[i].dob.date)}`
+    
+    // show the modal & display its data
+    modalContainerDiv.hidden = ''
+    modalInfoContainerDiv.innerHTML = `
+        <img class="modal-img" src=${pic} alt=${name} Photo>
+        <h3 class="modal-name cap">${name}</h3>
+        <p class="modal-text">${email}</p>
+        <p class="modal-text cap">${city}</p>
+        <hr>
+        <p class="modal-text">${cellNo}</p>
+        <p class="modal-text">${address}</p>
+        <p class="modal-text">${birthday}</p>
+    `
+
+    // if modalEmpIndex reach the first, or the last index, then hide prev/next button
+    switch (modalEmpIndex) {
+        case 0:
+            modalPrevBtn.style.display = 'none'
+            break;
+        case employeeQty-1:
+            modalNextBtn.style.display = 'none'
+            break;
+        default:
+            modalPrevBtn.style.display = ''
+            modalNextBtn.style.display = ''
+            break;
+    }
+}
+
+
+/**
+ * This function will format the string passed to it into phone number format
+ * (XXX) XXX-XXXX
+ * @param {string} string - phone number to be formatted
+ * @returns formatted cell phone number
+ */
+function formatCellNo(string) {
+    // 1. clean the passed argument from whitespaces, "-" , "(", and ")"
+    const unwantedChar = /[\s-()]+/g
+    const cleanCellNo = string.replace(unwantedChar,'')
+
+    // 2. group and capture its value
+    // 3. format the cleanCellNo into desired format
+    const phonePattern = /(\d{3})(\d{3})(\d{4})/
+    const formattedCellNo = cleanCellNo.replace(phonePattern, '($1) $2-$3 ')
+
+    return formattedCellNo
+}
+
+
+/**
+ * This function will format the string passed to it into birthday format
+ * MM/DD/YYYY
+ * @param {string} string - date value 
+ */
+function formatBirthday(string) {
+    const birthdayPattern = /(\d{4})-(\d{2})-(\d{2})(.*)/
+    const formattedBirthday = string.replace(birthdayPattern,'$2/$3/$1')
+
+    return formattedBirthday
+}
+//#endregion
+
+//#region Event Listener **************************************
+galleryDiv.addEventListener('click', (e) => {
+    const containerRegex = /^card-.+-container$/
+    let employeeCardDiv
+
+    if (containerRegex.test(e.target.className)) {
+        employeeCardDiv = e.target.parentNode
+    }
+    else {
+        employeeCardDiv = e.target.parentNode.parentNode
+    }
+
+    const employeeEmail = employeeCardDiv.id
+
+    // find the position of the clicked employee in employeeList
+    // and then generate modal to show employee's detail
+    for (let i = 0; i < employeeQty; i++){
+        if (employeeList[i].email === employeeEmail) {
+            modalEmpIndex = i
+            generateModal(modalEmpIndex)
+        }
+    }
+})
+
+
+modalCloseBtn.addEventListener('click', () => {
+    modalContainerDiv.hidden = 'true'
+    modalInfoContainerDiv.innerHTML = ''
+})
+
+
+searchInput.addEventListener('keyup', (e) => {
+    // convert userInput into searchRegex so that the search feature becomes very flexible
+    // Example: /.*an.+so.*/i --> will match for 'Joanne Simpson', and 'Hannah Wilson'
+    const space = /\s/g
+    const userInput = e.target.value
+    const searchRegex = new RegExp(`.*${userInput.replace(space,'.+')}.*`, 'i')
+
+    const nameOnCards = document.querySelectorAll('h3.card-name')    
+    for (let i = 0; i < nameOnCards.length; i++) {
+        const cardDiv = nameOnCards[i].parentNode.parentNode
+        
+        if(searchRegex.test(nameOnCards[i].textContent)) {
+            cardDiv.style.display = ''
+        }
+        else {
+            cardDiv.style.display = 'none'
+        }
+    }
+})
+
+modalNextBtn.addEventListener('click', () => {
+    modalEmpIndex++
+    generateModal(modalEmpIndex)
+})
+
+modalPrevBtn.addEventListener('click', () => {
+    modalEmpIndex--
+    generateModal(modalEmpIndex)
+})
+//#endregion
 
 
 
 
-
-
-// -------------------- ini semua perlu di buat secara dynamic
-
-
-// const modalContainerDiv = appendTo(body, 'div', {
-//     className: 'modal-container'
-// })
-
-// const modalDiv = appendTo(modalContainerDiv, 'div', {
-//     className: 'modal'
-// })
-
-// // const modalCloseBtn = 
-// appendTo(modalDiv, 'button', {
-//     type: 'button',
-//     id: 'modal-close-btn',
-//     className: 'modal-close-btn',
-//     innerHTML: '<strong>X</strong>'
-// })
-
-// const modalInfoDiv = appendTo(modalDiv, 'div', {
-//     className: 'modal-info-container'
-// })
-
-// appendTo(modalInfoDiv, 'img', {
-//     className: 'modal-img',
-//     src: 'https://placehold.it/125x125',
-//     alt: 'profile picture'
-// })
-
-// appendTo(modalInfoDiv, 'h3', {
-//     id: 'name',
-//     className: 'modal-name cap',
-//     textContent: 'name'
-// })
-
-// appendTo(modalInfoDiv, 'p', {
-//     className: 'modal-text',
-//     textContent: 'email'
-// })
-
-// appendTo(modalInfoDiv, 'p', {
-//     className: 'modal-text cap',
-//     textContent: 'city'
-// })
-
-// appendTo(modalInfoDiv, 'hr')
-
-// appendTo(modalInfoDiv, 'p', {
-//     className: 'modal-text',
-//     textContent: '(555) 555-5555'
-// })
-
-// appendTo(modalInfoDiv, 'p', {
-//     className: 'modal-text',
-//     textContent: '123 Portland Ave., Portland, OR 97204'
-// })
-
-// appendTo(modalInfoDiv, 'p', {
-//     className: 'modal-text',
-//     textContent: 'Birthday: 10/21/2015'
-// })
-
-// const modalBtnContainerDiv = appendTo(modalContainerDiv, 'div', {
-//     className: 'modal-btn-container'
-// })
-
-// appendTo(modalBtnContainerDiv, 'button', {
-//     type: 'button',
-//     id: 'modal-prev',
-//     className: 'modal-prev btn',
-//     textContent: 'Prev'
-// })
-
-// appendTo(modalBtnContainerDiv, 'button', {
-//     type: 'button',
-//     id: 'modal-next',
-//     className: 'modal-next btn',
-//     textContent: 'Next'
-// })
 
---
+++ readme.md
@@ -0,0 +1,12 @@
+Extra Credit:
+
+1. Search Feature:
+   - I intentionally delete the submit button from the HTML 
+     <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+   - Instead, the search feature will update dynamically on "keyup" event
+
+2. Modal toggle:
+   - To prevent errors, once the end or beginning of the list is reached, the prev/next button will be hidden
+
+3. Structure, style and CSS:
+   - when the card is being hovered, the background color of the card turn lightgreen
